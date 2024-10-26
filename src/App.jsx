a5022a9f8b4f1ce25645ff48a6951f41d1a0a4f8
Urim Rexhepi <63608973+darewill@users.noreply.github.com>
2024-10-26 14:10:39 +0200

    project created

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './css/App.css';
+
+export default function App() {
+  return (
+    <div>
+        <h1 className='text-3xl font-bold underline'>Revnaut</h1>
+    </div>
+  )
+}
